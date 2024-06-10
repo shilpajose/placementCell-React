81c262e2b683911bd9430cab7df1c4cfd92d8d0a
shilpajose <79206840+shilpajose@users.noreply.github.com>
2024-06-10 11:53:00 +0530

    Update SERVER_URL.js

--- src/Services/SERVER_URL.js
+++ src/Services/SERVER_URL.js
@@ -1,2 +1,2 @@
 // export const SERVER_URL = "https://placementcell-react-backend.onrender.com"
-export const SERVER_URL = "https://crud-react-server-9e12.onrender.com"
+export const SERVER_URL = "https://placementcell-react-backend.onrender.com"
